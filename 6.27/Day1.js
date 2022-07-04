320b669717c85bc38c94a64156f23da70258aa45
Davidup1 <liyujie2003@bupt.edu.cn>
2022-07-04 14:59:07 +0800

    第一个文件夹

---
+++ 6.27/Day1.js
@@ -0,0 +1,16 @@
+// var age; //声明age变量
+// // 弹出输入框
+// prompt('请输入你的年龄：');
+// // 弹出警示框
+// alert('那么计算的年龄是');
+// // 控制台输出
+// console.log('我是程序员看到的');
+// myname = prompt('请输入你的名字：');
+// alert('欢迎你，'+myname+'！');
+// str = prompt(myname+'，请输入你的年龄：');
+// age = 2022-Number(str)+1;
+// alert(myname+'，你今年'+String(age)+'岁了。');
+myname = prompt('来着何人？');
+gender = prompt('男的女的？');
+age = prompt('今年几岁啊?');
+alert('你的名字是：'+myname+'\n'+'你的性别是：'+gender+'\n'+'你的年龄是：'+age);
