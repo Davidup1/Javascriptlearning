7bc5735720d107185e1893a01fd95aebbabe7ea2
Davidup1 <liyujie2003@bupt.edu.cn>
2022-07-11 16:03:59 +0800

    学习了窗口、元素、键盘、鼠标事件

--- 6.27/Day2.js
+++ 6.27/Day2.js
@@ -40,19 +40,135 @@
 // a.href = "https://www.baidu.com";
 // console.log(a.href);
 
-var ul = document.createElement("ul");
+// var ul = document.createElement("ul");
 
-var li1 = document.createElement("li");
-var text1 = document.createTextNode("列表1");
-li1.appendChild(text1);
+// var li1 = document.createElement("li");
+// var text1 = document.createTextNode("列表1");
+// li1.appendChild(text1);
 
-var li2 = document.createElement("li");
-var text2 = document.createTextNode("列表2");
-li2.appendChild(text2);
+// var li2 = document.createElement("li");
+// var text2 = document.createTextNode("列表2");
+// li2.appendChild(text2);
 
-ul.appendChild(li1);
-ul.appendChild(li2);
+// ul.appendChild(li1);
+// ul.appendChild(li2);
 
-var uL = document.getElementById("a");
-var b = document.getElementById("body");
-b.insertBefore(ul,uL);
+// var uL = document.getElementById("a");
+// var b = document.getElementById("body");
+// b.insertBefore(ul,uL);
+
+// window.onload = function () {
+//     alert("hello world!");
+// }
+
+// window.onresize = function () {
+//     alert("窗口大小正在改变。");
+// }
+//当文本框获取焦点，文本框背景为红色，当文本框失去焦点，文本框背景为白色
+// var text = document.getElementById("text");
+// text.onfocus = function ()
+// {
+//     text.style.backgroundColor = "red";
+// }
+
+// text.onblur = function ()
+// {
+//     this.style.backgroundColor = "white";
+// }
+// // //文本框内容改变时，鼠标离开文本框（所谓的离开文本框也就是文本框失焦），自动将文本框的内容输出到控制台
+// // text.onchange = function ()
+// // {
+// //     console.log(this.value);
+// // }
+// // //当文本框内容改变时，立即将改变的内容输出到控制台
+// // text.oninput = function ()
+// // {
+// //     console.log(this.value)
+// // }
+// //
+// var sub = document.getElementById("submit");
+
+// sub.onclick = function ()
+// {
+//     if( text.value == "")
+//     {
+//         alert("喂，什么都不写总不太好的");
+//     }
+//     else 
+//     {
+//         console.log(text.value);
+//         alert("表单已提交");
+//     }
+// }
+
+// window.onkeydown = function (event)
+// {
+//     var speed = 100;
+//     var event = event;
+//     if (event.keyCode == 37)
+//     {
+//         if(event.ctrlKey)
+//         {
+//             alert("松开‘ctrl’");
+//         }
+//         box.style.left = box.offsetLeft - speed +"px";
+//     }
+//     if (event.keyCode == 39)
+//     {
+//         if(event.ctrlKey)
+//         {
+//             alert("松开‘ctrl’");
+//         }
+//         box.style.left = box.offsetLeft + speed +"px";
+//     }
+//     if (event.keyCode == 38)
+//     {
+//         if(event.ctrlKey)
+//         {
+//             alert("松开‘ctrl’");
+//         }
+//         box.style.top = box.offsetTop - speed +"px";
+//     }
+//     if (event.keyCode == 40)
+//     {
+//         if(event.ctrlKey)
+//         {
+//             alert("松开‘ctrl’");
+//         }
+//         box.style.top = box.offsetTop + speed +"px";
+//     }
+
+// }
+
+var box = document.getElementById("vital");
+box.onmouseenter = function()
+{
+    box.style.backgroundColor = "green";
+}
+
+box.onmouseleave = function ()
+{
+    box.style.backgroundColor = "red";
+}
+
+box.onmousedown = function(event)
+{
+    var event = event;
+
+    var ol = event.clientX - box.offsetLeft;
+    var ot = event.clientY - box.offsetTop;
+
+    box.onmousemove = function (event)
+    {
+        var event = event;
+        var left = event.clientX - ol;
+        var top = event.clientY - ot;
+
+        box.style.left = left + "px";
+        box.style.top = top + "px";
+    };
+    box.onmouseup = function()
+    {
+        box.onmousemove = null;
+    }  //防止松开之后还能拖动
+}
