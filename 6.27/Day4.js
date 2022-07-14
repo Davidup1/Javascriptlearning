f62f2c697308c57972ecc7521a5994cd3ab865bc
Davidup1 <liyujie2003@bupt.edu.cn>
2022-07-14 15:45:44 +0800

    学完了窗口的BOM

---
+++ 6.27/Day4.js
@@ -0,0 +1,24 @@
+// if(confirm("真的要进入这个网站吗？"))
+// {
+//     console.log("欢迎欢迎");
+// }
+// else
+// {
+//     console.log("拜拜了您内！");
+// }
+var person = prompt("请输入你的名字","爱吃炸鱼的薯条");
+if(person != null)
+    {
+        console.log(person);
+    }
+
+// var btn = document.getElementById("btn");
+
+// btn.onclick = function()
+// {
+//     var timer = setInterval(function(){
+//         console.log("hello world");
+//     },1000);
+// }
+
+window.resizeTo(100,100);
